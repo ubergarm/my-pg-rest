505713d8ef77b3b3a848eb2af6b8cd10f7895dbe
John Leimgruber <leimgrub@gmail.com>
2016-07-12 09:52:40 -0400

    SIGHUP PostgREST after migrating schema.

--- import.sh
+++ import.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 [[ $# -ne 1 ]] && echo WARNING - DESTRUCTIVE COMMAND. Usage: $0 to-import.sql && exit
 docker run -it --rm -v `pwd`/$1:/import.sql --link mypgrest_db_1:postgres postgres psql -h postgres -U postgres -f /import.sql
+# always signal HUP to PostgREST after migrating schema
+docker kill --signal=HUP mypgrest_api_1
